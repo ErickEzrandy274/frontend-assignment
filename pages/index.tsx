fe19574411fc0629cdee604c323e285be22e415a
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-04 21:54:30 +0700

    refactor: give title page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import { HomePage } from "@modules";
+import Head from "next/head";
 import React from "react";
 
 const Home = () => {
-	return <HomePage />;
+	return (
+		<>
+			<Head>
+				<title>OnlineStore</title>
+			</Head>
+			<HomePage />
+		</>
+	);
 };
 
 export default Home;
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import React from "react";
 
 const Search = () => {
-	return <div>Search</div>;
+	return (
+		<>
+			<Head>
+				<title>OnlineStore | Search Product</title>
+			</Head>
+			<div>Search</div>
+		</>
+	);
 };
 
 export default Search;
