9b4ee12b5935cd10ee869d564393354291de9032
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-04 21:36:39 +0700

    refactor: refactor logic after clicking mobile's navbar

--- components/elements/Navbar/index.tsx
+++ components/elements/Navbar/index.tsx
@@ -5,37 +5,37 @@ import {
 	HStack,
 	IconButton,
 	useDisclosure,
-	useColorModeValue,
 	Stack,
 	Heading,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import { routeList } from "./constant";
 import { NavLinkProps } from "./interface";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
-const NavLink: React.FC<NavLinkProps> = ({ name, url }) => {
-	const { pathname } = useRouter();
+const NavLink: React.FC<NavLinkProps> = ({ name, url, onClose }) => {
+	const { pathname, push } = useRouter();
+	const handleClick = () => {
+		push(url);
+		onClose();
+	};
 
 	return (
-		<Link href={url}>
-			<Box
-				px={2}
-				py={1}
-				rounded={"md"}
-				_hover={{
-					textDecoration: "none",
-					bg: "gray.500",
-					color: "gray.200",
-				}}
-				bg={pathname === url ? "gray.500" : "transparent"}
-				color={pathname === url ? "gray.200" : "black"}
-				fontWeight={pathname === url ? "semibold" : "medium"}
-			>
-				{name}
-			</Box>
-		</Link>
+		<Box
+			onClick={handleClick}
+			px={2}
+			py={1}
+			rounded={"md"}
+			_hover={{
+				bg: "gray.500",
+				color: "gray.200",
+			}}
+			bg={pathname === url ? "gray.500" : "transparent"}
+			color={pathname === url ? "gray.200" : "black"}
+			fontWeight={pathname === url ? "semibold" : "medium"}
+		>
+			{name}
+		</Box>
 	);
 };
 
@@ -43,19 +43,15 @@ const Navbar = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<Box bg="cyan.500" px={4}>
+		<Box bg="cyan.500" px={4} position="fixed" top={0} zIndex={2} w="full">
 			<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
 				<HStack spacing={8} alignItems={"center"}>
-					<Heading
-						bgGradient="linear(to-br, #facc15, #ec4899)"
-						bgClip="text"
-						textTransform="uppercase"
-					>
-						Sathi
+					<Heading bgGradient="linear(to-br, #facc15, #ec4899)" bgClip="text">
+						OnlineStore
 					</Heading>
 					<HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
 						{routeList.map((link) => (
-							<NavLink key={link.url} {...link} />
+							<NavLink key={link.url} {...link} onClose={onClose} />
 						))}
 					</HStack>
 				</HStack>
@@ -73,7 +69,7 @@ const Navbar = () => {
 				<Box pb={4} display={{ md: "none" }}>
 					<Stack as={"nav"} spacing={2}>
 						{routeList.map((link) => (
-							<NavLink key={link.url} {...link} />
+							<NavLink key={link.url} {...link} onClose={onClose} />
 						))}
 					</Stack>
 				</Box>
--- components/elements/Navbar/interface.ts
+++ components/elements/Navbar/interface.ts
@@ -1,4 +1,5 @@
 export interface NavLinkProps {
 	name: string;
 	url: string;
+	onClose: () => void;
 }
