7e80188bf6deb4331dfeea6ed9ab8bc3354d09c7
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-04 20:05:29 +0700

    refactor: wrap origin component with chakra provider and main layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { MainLayout } from "@modules";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
+	return (
 		<ChakraProvider>
-			<Component {...pageProps} />
+			<MainLayout>
+				<Component {...pageProps} />
+			</MainLayout>
 		</ChakraProvider>
 	);
 }
