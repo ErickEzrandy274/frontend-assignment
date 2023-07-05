a45b2d88ca551da3fb826a1abb0ff3708e8000be
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-05 12:08:24 +0700

    chore: move to helper method dir

--- components/utils/helperMethod/fetchProduct.ts
+++ components/utils/helperMethod/fetchProduct.ts
@@ -4,7 +4,6 @@ import toast from "react-hot-toast";
 export const getProducts = async () => {
 	toast.loading("Fetching all products...");
 	try {
-		console.log(`${process.env.NEXT_PUBLIC_URL}/products`);
 		const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/products`);
 		toast.dismiss();
 		toast.success("Successfully fetching all products!");
