de142faf2c5c740d786e2b6ed2047bca1f79ee09
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-05 15:24:31 +0700

    chore: remove toast loading

--- components/utils/helperMethod/fetchProduct.ts
+++ components/utils/helperMethod/fetchProduct.ts
@@ -2,14 +2,23 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 export const getProducts = async () => {
-	toast.loading("Fetching all products...");
 	try {
 		const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/products`);
-		toast.dismiss();
 		toast.success("Successfully fetching all products!");
 		return data;
 	} catch (error: any) {
-		toast.dismiss();
+		toast.error(error.message);
+	}
+};
+
+export const getProductById = async (id: number) => {
+	try {
+		const { data } = await axios.get(
+			`${process.env.NEXT_PUBLIC_URL}/products/${id}`
+		);
+		toast.success("Successfully fetching detail product!");
+		return data;
+	} catch (error: any) {
 		toast.error(error.message);
 	}
 };
