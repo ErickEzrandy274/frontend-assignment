c21561ae58012d9e13a7da18a1adcf942c58fff5
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-05 16:11:05 +0700

    refactor: add functionality of detail button

--- components/elements/Card/ProductCard.tsx
+++ components/elements/Card/ProductCard.tsx
@@ -1,17 +1,26 @@
 import {
 	Box,
-	Center,
 	useColorModeValue,
 	Heading,
 	Text,
 	Stack,
 	Image,
+	Button,
+	Flex,
 } from "@chakra-ui/react";
 import { ProductCardProps } from "./interface";
+import { useRouter } from "next/router";
 
 const ProductCard: React.FC<ProductCardProps> = (props) => {
+	const { push } = useRouter();
+	const handleClick = () => {
+		push(`/product/${props.id}`);
+	};
+
 	return (
-		<Box
+		<Flex
+			flexDir="column"
+			justifyContent="space-between"
 			role="group"
 			p={6}
 			maxW="330px"
@@ -33,18 +42,33 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
 					alt={props.title}
 				/>
 			</Box>
-			<Stack pt={10} align={"center"} textAlign="center">
-				<Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
-					{props.category}
-				</Text>
-				<Heading fontSize={"xl"} fontFamily={"body"} fontWeight={700}>
-					{props.title}
-				</Heading>
-				<Text fontWeight={800} fontSize={"xl"}>
-					${props.price}
-				</Text>
-			</Stack>
-		</Box>
+
+			<Flex flexDir="column" gap={5} justifyContent="space-between">
+				<Stack align={"center"} textAlign="center">
+					<Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
+						{props.category}
+					</Text>
+
+					<Heading fontSize={"xl"} fontFamily={"body"} fontWeight={700}>
+						{props.title}
+					</Heading>
+
+					<Text fontWeight={800} fontSize={"xl"}>
+						${props.price}
+					</Text>
+				</Stack>
+
+				<Flex justifyContent="end" alignItems="flex-end">
+					<Button
+						onClick={handleClick}
+						transition="ease-in-out .35s"
+						colorScheme="whatsapp"
+					>
+						See Detail
+					</Button>
+				</Flex>
+			</Flex>
+		</Flex>
 	);
 };
 
