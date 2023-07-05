c3231aff677b3254225abec0bf32dc0add66b8ae
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-07-05 12:06:31 +0700

    feat: create search bar component

---
+++ components/elements/SearchBar/index.tsx
@@ -0,0 +1,48 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input } from "@chakra-ui/react";
+import { useKeyword } from "@utils";
+import React, { BaseSyntheticEvent, useState } from "react";
+
+const SearchBar = () => {
+	const [keyword, setKeyword] = useState<string>("");
+	const { updateKeyword } = useKeyword();
+	const handleChange = (e: BaseSyntheticEvent) => {
+		setKeyword(e.target.value);
+	};
+
+	const handleSearch = (e: any) => {
+		if (e.key === "Enter") {
+			updateKeyword(keyword);
+		}
+	};
+
+	return (
+		<Flex
+			alignItems="center"
+			border="2px solid white"
+			px={3}
+			py={2}
+			w="72"
+			gap={3}
+			rounded="xl"
+		>
+			<SearchIcon />
+
+			<Input
+				value={keyword}
+				border="none"
+				variant="unstyled"
+				onKeyDown={handleSearch}
+				onChange={handleChange}
+				placeholder="Find the product here..."
+				sx={{
+					"&::placeholder": {
+						color: "black", // Set the desired color here
+					},
+				}}
+			/>
+		</Flex>
+	);
+};
+
+export default SearchBar;
